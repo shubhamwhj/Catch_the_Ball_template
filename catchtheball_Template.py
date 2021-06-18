01b3733dde71d0e33e70e7ebfc55ea58e2e4867f
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2021-06-18 17:45:16 +0530

    Add files via upload

---
+++ catchtheball_Template.py
@@ -0,0 +1,52 @@
+import pygame, sys, random
+
+#initialize pygame to  use it's functions
+pygame.init()
+clock=pygame.time.Clock()
+
+#create a window where game will Run
+screen = pygame.display.set_mode((400,400))
+#title 
+pygame.display.set_caption("Catch the Ball")
+
+#load images
+background_image = pygame.image.load("bg.png").convert()
+plr_img = pygame.image.load("player.png").convert_alpha()
+plr_img=pygame.transform.smoothscale(plr_img,(60,90))
+ball_image = pygame.image.load("ball.png").convert_alpha()
+ball_image=pygame.transform.smoothscale(ball_image,(40,40))
+over_img=pygame.image.load("over.png").convert_alpha()
+over_img=pygame.transform.smoothscale(over_img,(200,100))
+
+#creating objects of game
+ball=pygame.Rect(200,0,40,40)
+player=pygame.Rect(100,310,60,90)
+
+
+while True:
+  
+  screen.blit(background_image,[0,0])
+  #event loop to check which key is print
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+    #Code here, use events to move the player   
+    
+  
+ 
+    
+  #code for ball falling
+  ball.y=ball.y+5
+  if(ball.colliderect(player)):
+      ball.y=0
+      ball.x=random.randint(0, 360)
+  if(ball.y>400):
+      screen.blit(over_img,[100,100])
+      
+      
+  screen.blit(plr_img,player)
+  screen.blit(ball_image,ball)
+  
+  pygame.display.flip()
+  clock.tick(30)
